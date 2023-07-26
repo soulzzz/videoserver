22c1d5c0cbc94705914d1aec58c13f033148e987
esoterik_xiao <soulxzb@gmail.com>
2023-07-26 21:13:33 +0800

    videoserver commit

---
+++ src/main/java/com/soul/videoserver/VideoserverApplication.java
@@ -0,0 +1,13 @@
+package com.soul.videoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soul/videoserver/VideoserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soul.videoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
